205025c89725ecdb1fd2f41b1a440e234cc173a8
Darshan Heble <darshanheble@gmail.com>
2024-04-25 18:58:04 +0530

    ..

--- app/lib/screens/bell_screen.dart
+++ app/lib/screens/bell_screen.dart
@@ -8,6 +8,40 @@ class BellScreen extends StatelessWidget {
     return Scaffold(
         body: Stack(
       children: [
+        Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: ListView.builder(
+              itemCount: 10,
+              itemBuilder: (context, index) {
+                return Expanded(
+                    flex: 10,
+                    child: Row(
+                      children: [
+                        Card(
+                          child: Center(
+                            child: Text("hello $index"),
+                            widthFactor: 4,
+                            heightFactor: 10,
+                          ),
+                        ),
+                      ],
+                    ));
+              }),
+          // child: GridView.count(
+          //   mainAxisSpacing: 15,
+          //   crossAxisSpacing: 15,
+          //   crossAxisCount: 3,
+          //   children: List.generate(10, (index) {
+          //     return Card(
+          //       child: Center(
+          //         child: Text("text $index"),
+          //         heightFactor: 100,
+          //         widthFactor: 100,
+          //       ),
+          //     );
+          //   }),
+          // ),
+        ),
         Positioned(
           bottom: 50,
           right: 50,
