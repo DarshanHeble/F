ba6543d37ea1e51a65c80ec2420ce77d79b24f41
Darshan Heble <darshanheble@gmail.com>
2024-04-28 20:13:05 +0530

    ..

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:app/screens/main_screen.dart';
+import 'package:app/screens/splash_screen.dart';
 import 'package:app/themes/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -14,111 +16,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      themeMode: ThemeMode.system,
-      // theme: ThemeData(
-      //   scaffoldBackgroundColor: const Color(0xFF15131C),
-      //   colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      //   useMaterial3: true,
-      // ),
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        themeMode: ThemeMode.system,
+        // theme: ThemeData(
+        //   scaffoldBackgroundColor: const Color(0xFF15131C),
+        //   colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        //   useMaterial3: true,
+        // ),
 
-      theme: TAppTheme.lightTheme,
-      darkTheme: TAppTheme.darkTheme,
-
-      // home: const MyHomePage(title: 'Flutter Demo Home Page'),
-      home: const MainScreen(),
-    );
+        theme: TAppTheme.lightTheme,
+        darkTheme: TAppTheme.darkTheme,
+        home: SplashScreen());
   }
 }
-
-// class MyHomePage extends StatefulWidget {
-//   const MyHomePage({super.key, required this.title});
-
-//   final String title;
-
-//   @override
-//   State<MyHomePage> createState() => _MyHomePageState();
-// }
-
-// class _MyHomePageState extends State<MyHomePage> {
-//   int _counter = 0;
-
-//   void _incrementCounter() {
-//     setState(() {
-//       _counter++;
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       // appBar: AppBar(
-//       //   backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-//       //   title: Text(widget.title),
-//       // ),
-//       body: Stack(
-//         children: [
-//           Center(
-//             child: Text("helllo"),
-//           ),
-//           sidebar(),
-//         ],
-//       ),
-//       // body: Row(
-//       //   children: [
-//       //     SidebarX(
-//       //       controller: SidebarXController(selectedIndex: 0),
-//       //       items: [
-//       //         SidebarXItem(
-//       //           icon: Icons.notifications,
-//       //           label: 'home',
-//       //           onTap: () {
-//       //             debugPrint("hello");
-//       //           },
-//       //         ),
-//       //         SidebarXItem(icon: Icons.volume_up, label: 'Announcement'),
-//       //         SidebarXItem(icon: Icons.settings, label: 'setting'),
-//       //       ],
-//       //     )
-//       //   ],
-//       // ),
-//       // body: Center(
-//       //   child: Column(
-//       //     mainAxisAlignment: MainAxisAlignment.center,
-//       //     children: <Widget>[
-//       //       const Text(
-//       //         'You have pushed the button this many times:',
-//       //       ),
-//       //       Text(
-//       //         '$_counter',
-//       //         style: Theme.of(context).textTheme.headlineMedium,
-//       //       ),
-//       //     ],
-//       //   ),
-//       // ),
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: _incrementCounter,
-//         tooltip: 'Increment',
-//         child: const Icon(Icons.add),
-//       ),
-//     );
-//   }
-// }
-
-// class sidebar extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Visibility(
-//       visible: true,
-//       child: Column(
-//         children: [
-//           ElevatedButton(
-//               onPressed: () {},
-//               child: Icon(Icons.home),
-//               style: ElevatedButton.styleFrom())
-//         ],
-//       ),
-//     );
-//   }
-// }
---
+++ app/lib/screens/splash_screen.dart
@@ -0,0 +1,19 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:app/const/constant.dart';
+import 'package:app/screens/main_screen.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSplashScreen(
+      splash: "assets/Bell_logo.png",
+      splashIconSize: double.tryParse("400"),
+      duration: 200,
+      backgroundColor: black,
+      nextScreen: MainScreen(),
+    );
+  }
+}
