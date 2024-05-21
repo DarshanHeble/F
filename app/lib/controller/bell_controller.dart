20c9e8ca5688a5418fdebd458a399d0f830f2c9f
Darshan Heble <darshanheble@gmail.com>
2024-05-21 16:58:55 +0530

    n

--- app/lib/controller/bell_controller.dart
+++ app/lib/controller/bell_controller.dart
@@ -47,17 +47,17 @@ class BellController extends GetxController {
   late EmployeeModel employeeModel;
   var itemCount = 2.obs;
 
-  @override
-  void onInit() {
-    // TODO: implement onInit
-    super.onInit();
-  }
+  // @override
+  // void onInit() {
+  //   // TODO: implement onInit
+  //   super.onInit();
+  // }
 
-  @override
-  void onReady() {
-    // TODO: implement onReady
-    super.onReady();
-  }
+  // @override
+  // void onReady() {
+  //   // TODO: implement onReady
+  //   super.onReady();
+  // }
 
   @override
   void onClose() {
